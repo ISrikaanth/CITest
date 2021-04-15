495c605d8d7426cc0ce852c0effbd8552f734f9b
ISrikaanth <srikanth.indrakanti0@gmail.com>
2021-04-15 14:48:36 +0530

    removed route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 /**
  * GET /status
  */
-router.get('/api/status', (req, res) => res.send('OKK'));
+router.get('/api/status', (req, res) => res.send('OK'));
 
 module.exports = router;
